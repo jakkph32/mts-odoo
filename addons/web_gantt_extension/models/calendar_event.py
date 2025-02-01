be5b973e8806a0e83aa64c9b446e6ae6becb6259
Jakkraphop Pengchan <jakkph32@gmail.com>
2025-02-02 03:37:38 +0700

    remove unused appointment gantt extension files and add calendar navigation module

--- addons/web_gantt_extension/models/calendar_event.py
+++
@@ -1,7 +0,0 @@
-from odoo import models, fields
-
-
-class CalendarEvent(models.Model):
-    _inherit = 'calendar.event'
-
-    appointment_status = fields.Selection(selection_add=[('invoice', 'Invoiced')])
